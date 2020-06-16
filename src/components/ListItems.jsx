45590c774bb4077f0f5eab1be5d70c778bb09ece
Denis Mory <denismr7@gmail.com>
2020-06-16 11:18:51 +0200

    ItemList with api call and added userdata to json

--- src/components/ListItems.jsx
+++ src/components/ListItems.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import ProductItem from './ProductItem'
-import Data from '../data/data'
 import {StyledItemsCont} from './styles/StyledListItems'
+import { useState } from 'react'
 
 function ListItems(props) {
-    const filteredArray = Data.filter(element => element.name.toLowerCase()
-    .includes(props.search.toLowerCase())).map(data => <ProductItem data={data} key={data.id}/>)
+    const [products, setProducts] = useState()
+    let filteredArray = []
+    fetch("https://my-json-server.typicode.com/Denismr7/react-search-products/Data").then(response => response.json()).then(data => setProducts(data))
+    if (products !== undefined) { 
+        filteredArray = products.filter(product => product.name.toLowerCase()
+        .includes(props.search.toLowerCase())).map(data => <ProductItem data={data} key={data.id}/>)
+    }
+    else {
+        filteredArray = "Loading"
+    }
     return (
         <StyledItemsCont>
             {filteredArray}
